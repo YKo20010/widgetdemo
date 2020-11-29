0d5538cff26860d199d3ae20d94d8af60b6833eb
yk396 <yk396@cornell.edu>
2020-11-29 15:34:53 -0500

    Update

--- Demo/Demo.swift
+++ Demo/Demo.swift
@@ -26,7 +26,7 @@ struct Provider: TimelineProvider {
         let currentDate = Date()
         for offset in 1 ..< 60 {
             let entryDate = Calendar.current.date(byAdding: .second, value: offset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, image: "snowman\(Int.random(in: 1..<5))")
+            let entry = SimpleEntry(date: entryDate, image: "snowman\(Int.random(in: 1..<8))")
             entries.append(entry)
         }
 
@@ -40,64 +40,46 @@ struct SimpleEntry: TimelineEntry {
     let image: String
 }
 
-struct Number {
-    var title: String
-    var number: Int
-}
-
 struct DemoEntryView : View {
     // NOTE: extract widget size
     @Environment(\.widgetFamily) var size
     @State var snowman: Int = Global.getSnowman()
     
     var entry: Provider.Entry
-    
-    let items: [String] = ["A", "B", "C"]
-    let numbers: [Number] = [
-        Number(title: "One", number: 1),
-        Number(title: "Two", number: 2),
-        Number(title: "Three", number: 3),
-        Number(title: "Four", number: 4),
-        Number(title: "Five", number: 5),
-    ]
 
     var body: some View {
-//        Text(entry.date, style: .time)
-//        Text(entry.date, style: .date)
         switch size {
-//            case .systemSmall:
-//                Text("Small Widget")
-//                    .foregroundColor(Color.red)
-            case .systemMedium:
-                VStack(alignment: .leading) {
-                    Text("Medium Widget")
-                        .foregroundColor(Color.green)
-                    Image("snowman\(snowman)")
+            case .systemSmall:
+                ZStack(alignment: .center) {
+                    Image(entry.image)
                         .resizable()
                         .scaledToFill()
+                    // NOTE: takes shape of container
+                    ContainerRelativeShape()
+                        .fill(Color.black)
+                        .opacity(0.5)
+                    VStack(alignment: .center, spacing: nil) {
+                        Text(entry.date, style: .time)
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                        Text(entry.date, style: .date)
+                            .font(.caption)
+                            .foregroundColor(.white)
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(width: 50, height: 1)
+                        
+                    }
                 }
+            case .systemMedium:
+                Image("snowman\(snowman)")
+                    .resizable()
+                    .scaledToFill()
             case .systemLarge:
                 Text("Large Widget")
                     .foregroundColor(Color.blue)
             default:
-                VStack(alignment: .leading) {
-                    Text("Hello")
-                        .foregroundColor(Color.blue)
-                        .background(LinearGradient(gradient: Gradient(colors: [.green, .orange]), startPoint: .leading, endPoint: .trailing))
-                    Text(entry.date, style: .time)
-                        .font(.largeTitle)
-                    Text(entry.date, style: .date)
-                        .font(.caption)
-                    HStack(alignment: .top, spacing: 10) {
-                        ForEach(numbers, id: \.title) {
-                            Text("\($0.number)")
-                                .font(.caption2)
-                        }
-                    }
-                    Image(entry.image)
-                        .resizable()
-                        .scaledToFit()
-                }
+                Text("Default Widget")
         }
     }
 }
@@ -110,11 +92,8 @@ struct Demo: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             DemoEntryView(entry: entry)
         }
-        // NOTE: Name and description shows where you add the widget.
-        .configurationDisplayName("Demo")
+        .configurationDisplayName("Snowmen")
         .description("Happy holidays.")
-        // NOTE: by default, supports all sizes. Can add this to restrict.
-//        .supportedFamilies([.systemSmall])
     }
 }
 
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,29 +7,42 @@
 
 import SwiftUI
 
+struct Snowman {
+    var id: String
+    var number: Int
+}
+
 struct ContentView: View {
     @State var snowman: Int = Global.getSnowman()
+    
+    let snowmen: [Snowman] = [
+        Snowman(id: "One", number: 1),
+        Snowman(id: "Two", number: 2),
+        Snowman(id: "Three", number: 3),
+        Snowman(id: "Four", number: 4),
+        Snowman(id: "Five", number: 5),
+        Snowman(id: "Six", number: 6),
+        Snowman(id: "Seven", number: 7),
+    ]
+    
     var body: some View {
-        Text("\(snowman)")
-            .padding()
-        VStack(alignment: .center, spacing: nil, content: {
-            Button("1") {
-                Global.setSnowman(i: 1)
-                snowman = 1
+        VStack(alignment: .leading) {
+            Text("Snowman \(snowman)")
+                .foregroundColor(Color.white)
+                .background(LinearGradient(gradient: Gradient(colors: [.blue, .gray]), startPoint: .leading, endPoint: .trailing))
+                .font(.largeTitle)
+            HStack(alignment: .center, spacing: 10) {
+                ForEach(snowmen, id: \.id) { s in
+                    Button("\(s.number)") {
+                        Global.setSnowman(i: s.number)
+                        snowman = s.number
+                    }
+                }
             }
-            Button("2") {
-                Global.setSnowman(i: 2)
-                snowman = 2
-            }
-            Button("3") {
-                Global.setSnowman(i: 3)
-                snowman = 3
-            }
-            Button("4") {
-                Global.setSnowman(i: 4)
-                snowman = 4
-            }
-        })
+            Image("snowman\(snowman)")
+                .resizable()
+                .scaledToFit()
+        }.padding()
     }
 }
 
