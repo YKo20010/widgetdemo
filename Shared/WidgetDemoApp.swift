6bf90e2dcc42a7a154e256d175de6af5c92df627
yk396 <yk396@cornell.edu>
2020-11-28 19:26:24 -0500

    Initial Commit

---
+++ Shared/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  Shared
+//
+//  Created by Artesia Ko on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
